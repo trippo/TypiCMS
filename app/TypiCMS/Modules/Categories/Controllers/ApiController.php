3123d2a1605a722621449611fd73f9dd04c575b6
Alberto Peripolli <info@albertoperipolli.com>
2015-01-17 12:32:04 +0100

    up

--- app/TypiCMS/Modules/Categories/Controllers/ApiController.php
+++ app/TypiCMS/Modules/Categories/Controllers/ApiController.php
@@ -3,11 +3,34 @@ namespace TypiCMS\Modules\Categories\Controllers;
 
 use TypiCMS\Controllers\BaseApiController;
 use TypiCMS\Modules\Categories\Repositories\CategoryInterface as Repository;
+use Input;
+use Response;
 
 class ApiController extends BaseApiController
 {
     public function __construct(Repository $repository)
     {
         parent::__construct($repository);
+    }
+    
+    
+    /**
+     * Get models
+     * 
+     * @return Response
+     */	
+    public function index()
+    {
+	    $models=$this->repository->getCategoriesForList();
+        //$models = $this->repository->getAll([], true);
+        /*
+foreach($models as $model)
+        {
+	        
+		    echo "<br/>".$model->title;
+		    
+	    }
+*/
+        return Response::json($models, 200);
     }
 }
